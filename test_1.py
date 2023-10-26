10b0ff51ffcb1a736342e00e7afba710deb747bf
Anna-Gaidukova <anna.v.gaydukova@gmail.com>
2023-10-26 20:34:42 +0300

    test_1

---
+++ test_1.py
@@ -0,0 +1,8 @@
+class TestSuite:
+
+    def test_1(self, browser):
+        browser.get('https://casenik.com.ua/')
+
+
+    def test_2(self, browser):
+        browser.get('https://casenik.com.ua/')
