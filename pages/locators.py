34d158264e0ba40d48c4fd1e68c51d62e28b1362
Anna-Gaidukova <anna.v.gaydukova@gmail.com>
2023-11-02 20:38:08 +0200

    locators_add

--- pages/locators.py
+++ pages/locators.py
@@ -17,8 +17,22 @@ class BasePageLocators:
     SUBMIT_BUTTON = (By.XPATH, "//button[@type='submit']")
     WISHLIST = (By.XPATH, "//a[@href='wish/show']")
     CART = (By.XPATH, "//a[@href='cart/show']")
+    HOTHITS = (By.XPATH, '//a[@href="main/showHit" and @class="check-item check-hot"]')
+    SALES = (By.XPATH, '//a[@href="main/showSale"]/span[text()="Скидки"]')
+    NEW_COMINGS = (By.XPATH, '//a[@href="main/showNew"]/span[text()="Новинки"]')
+    READMI_LIST = (By.XPATH, '//div[text()="Readmi"]')
+    OPPO_LIST = (By.XPATH, '//div[text()="OPPO"]')
+    JBL_LIST = (By.XPATH, '//div[text()="JBL"]')
+    SAMSUNG_LIST = (By.XPATH, '//div[text()="Samsung"]')
+    SAMSUNG_J701 = (By.XPATH, '//a[text()="Samsung J701"]')
 
+    LENOVO_LIST = (By.XPATH, '//div[text()="Lenovo"]')
+    MEIZU_LIST = (By.XPATH, '//div[text()="Meizu"]')
+    HUAWEI_LIST = (By.XPATH, '//div[text()="Huawei"]')
+    XIAOMI_LIST = (By.XPATH, '//div[text()="Xiaomi"]')
+    APPLE_LIST = (By.XPATH, '//div[text()="Apple"]')
 
-
-
+    SUBSCRIBE = (By.XPATH, '//div[text()="Подпишитесь на рассылку"]')
+    NEWSLETTER_IMPUT = (By.XPATH, '//input[@name="submail"]')
+    FOOTER_LOGO = (By.XPATH, '//img[@src="images/logo-footer.png"]')
 
